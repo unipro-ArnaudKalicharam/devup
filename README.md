6ab7ed33c6a0d60a6e03c538cca9f3509578a591
Arnaud Kalicharam <arnaud.kalicharam@theuniprogroup.com>
2019-07-29 13:39:55 +0400

    Setting up initial ssh generator.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # devup
 Initial development setup
+
+This script is meant to be used on a Ubuntu machine.
+
+Note that this path can be accessed using this tinyurl:
+https://tinyurl.com/devup-sh
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "********************************"
+echo "* Setting up git authentication."
+echo "********************************"
+
+read -s "Please enter your git email: " gitemail
+read -s -p "Enter a passphrase - leave empty for no passphrase: " passphrase
+
+# Generate an SSH key without passphrase.
+yes y | ssh-keygen -t rsa -N "${passphrase}" -b 4096 -C "${gitemail}" -f ~/.ssh/id_rsa
+
+echo ""
+echo "********************************"
+echo "* Please add the ssh key contents from ~/.ssh/id_rsa.pub to your git account."
+echo "********************************"
+echo ""
